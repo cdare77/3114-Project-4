07870245a972e84bdffadbef18fc060966abef53
Chris Dare <cdare77@vt.edu>
2017-12-04 22:30:49 -0500

    Created TTNodeTest

---
+++ src/TTNodeTest.java
@@ -0,0 +1,70 @@
+import student.TestCase;
+
+/**
+ * Tests the functionality of TTNodes methods, including the
+ * private helper methods and all of their respective cases
+ * 
+ * @author Chris Dare (cdare77@vt.edu)
+ * @version 12/4/2017
+ */
+public class TTNodeTest extends TestCase {
+
+    private TTNode<Integer> root;
+    
+    @Override
+    public void setUp() {
+        root = new TTNode<Integer>(15, 25, null, null, null);
+    }
+    
+    /**
+     * tests the accessor methods for children and keys
+     */
+    public void testAccessor() {
+        assertEquals((int) root.lkey(), 15);
+        assertEquals((int) root.rkey(), 25);
+        
+        assertNull(root.leftChild());
+        assertNull(root.rightChild());
+        assertNull(root.centerChild());
+    }
+    
+    /**
+     * Tests the modifier methods for children and keys
+     */
+    public void testModifier() {
+        TTNode<Integer> flyweightChild = new TTNode<Integer>();
+        
+        root.setLeftKey(16);
+        assertEquals((int) root.lkey(), 16);
+        
+        root.setRightKey(17);
+        assertEquals((int) root.rkey(), 17);
+        
+        root.setLeftChild(flyweightChild);
+        root.setCenterChild(flyweightChild);
+        root.setRightChild(flyweightChild);
+        
+        assertEquals(root.leftChild(), flyweightChild);
+        assertEquals(root.rightChild(), flyweightChild);
+        assertEquals(root.centerChild(), flyweightChild);
+    }
+    
+    /**
+     * Tests the functionality of toString in the case of an
+     * empty node and nonempty node
+     */
+    public void testToString() {
+        assertEquals("15 25", root.toString());
+        assertEquals("null null", new TTNode<Integer>().toString());
+    }
+    
+    public void testRemove() {
+        root = root.insertHelp(20);
+        root = root.insertHelp(30);
+        root = root.insertHelp(27);
+        root = root.insertHelp(26);
+
+        assertEquals(30, (int) root.removeHelper(30));
+    }
+    
+}
